cf9b5c0f075fcf1947e051b713a3513c1cfce7e5
laneshetron <laneshetron@gmail.com>
2016-02-02 23:31:37 -0500

    Begin rate limiting only after 10 messages have been sent within the last 100ms

--- Bank/g.py
+++ Bank/g.py
@@ -2,22 +2,24 @@ import time
 import json
 import sqlite3
 import socket
+from collections import deque
 
 class safesocket(socket.socket):
     def __init__(self, *args):
-        self.lastSend = 0
+        self.floodQueue = deque([0] * 10, maxlen=10)
         super().__init__(*args)
 
     def send(self, message, *args):
         try:
-            elapsed = int(time.time()) - self.lastSend
+            elapsed = int(time.time()) - self.floodQueue[0]
             if not isinstance(message, bytes):
                 message = message.encode()
-            if elapsed < 100:
-                # Sleep between messages to avoid IRC kicking us for flooding
-                time.sleep((100 - elapsed) / 1000)
+            if elapsed / 10 < 100:
+                # Start rate limiting after 10 messages within 100ms
+                # to avoid IRC kicking us for flooding
+                time.sleep((100 - elapsed / 10) / 1000)
             super().send(message, *args)
-            self.lastSend = int(time.time())
+            self.floodQueue.append(int(time.time()))
         except Exception as e:
             print('Could not write to socket: ', e)
 
