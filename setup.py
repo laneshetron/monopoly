aae47fbb98520263e755ad739ef8ce31d951355c
laneshetron <laneshetron@gmail.com>
2016-02-20 00:50:12 -0500

    Add module setup/scaffolding

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='monopoly',
+      version='0.1.0',
+      description='A karma bot for Hangouts and IRC.',
+      long_description=readme(),
+      url='https://github.com/laneshetron/monopoly.git',
+      author='Lane Shetron',
+      author_email='laneshetron@gmail.com',
+      license='MIT',
+      packages=['monopoly'],
+      install_requires=['hangups'],
+      zip_safe=False)
+
+# Include setup of custom hangups branch
