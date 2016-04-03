9215cc21a36ded761683d6cc48d4fd9f926ac501
Lane Shetron <laneshetron@gmail.com>
2016-04-03 00:27:29 +0000

    Pull in most recent changes from Hangups

--- monopoly/Hangups.py
+++ monopoly/Hangups.py
@@ -7,6 +7,7 @@ import contextlib
 import logging
 import os
 import sys
+import urwid # unfortunate legacy garbage
 import readlike
 
 from Bank.Hangouts import Bank
@@ -19,6 +20,13 @@ from hangups.ui.notify import Notifier
 from hangups.ui.utils import get_conv_name
 from hangups.ui.utils import add_color_to_scheme
 
+
+# hangups used to require a fork of urwid called hangups-urwid which may still
+# be installed and create a conflict with the 'urwid' package name. See #198.
+if urwid.__version__ == '1.2.2-dev':
+    sys.exit('error: hangups-urwid package is installed\n\n'
+             'Please uninstall hangups-urwid and urwid, and reinstall '
+             'hangups.')
 
 CONVERSATION_TYPE_ONE_TO_ONE = 1
 CONVERSATION_TYPE_GROUP = 2
@@ -57,10 +65,11 @@ class ChatUI(object):
     """User interface for hangups."""
 
     def __init__(self, refresh_token_path, keybindings, palette,
-                 palette_colors, datetimefmt, disable_notifier):
+                 palette_colors, datetimefmt, notifier):
         """Start the user interface."""
         self._keys = keybindings
         self._datetimefmt = datetimefmt
+        self._notifier = notifier
 
         set_terminal_title('hangups')
 
@@ -108,7 +117,7 @@ class ChatUI(object):
         self.bank = Bank(*handlers, swift=swift)
 
     def _on_event(self, conv_event):
-        """Open conversation tab for new messages when they arrive."""
+        """Open conversation tab for new messages & pass events to notifier."""
         conv = self._conv_list.get(conv_event.conversation_id)
 
         user = conv.get_user(conv_event.user_id)
@@ -183,6 +192,9 @@ class ChatUI(object):
                 hangups.HANGOUT_EVENT_TYPE_END: (
                     'A Hangout call ended.'
                 ),
+                hangups.HANGOUT_EVENT_TYPE_ONGOING: (
+                    'A Hangout call is ongoing.'
+                ),
             }.get(conv_event.event_type, 'Unknown Hangout call event.')
             return MessageWidget(conv_event.timestamp, text, datetimefmt,
                                  show_date=is_new_day)
@@ -252,8 +264,6 @@ def main():
                       version='hangups {}'.format(hangups.__version__))
     general_group.add('-d', '--debug', action='store_true',
                       help='log detailed debugging messages')
-    general_group.add('-n', '--disable-notifications', action='store_true',
-                      help='disable desktop notifications')
     general_group.add('--log', default=default_log_path, help='log file path')
     key_group = parser.add_argument_group('Keybindings')
     key_group.add('--key-next-tab', default='ctrl d',
@@ -270,6 +280,13 @@ def main():
                   help='keybinding for alternate up key')
     key_group.add('--key-down', default='j',
                   help='keybinding for alternate down key')
+    notification_group = parser.add_argument_group('Notifications')
+    notification_group.add('-n', '--disable-notifications',
+                           action='store_true',
+                           help='disable desktop notifications')
+    notification_group.add('-D', '--discreet-notifications',
+                           action='store_true',
+                           help='hide message details in notifications')
 
     # add color scheme options
     col_group = parser.add_argument_group('Colors')
@@ -308,6 +325,11 @@ def main():
                                          getattr(args, 'col_' + name + '_bg'),
                                          palette_colors)
 
+    if not args.disable_notifications:
+        notifier = Notifier(args.discreet_notifications)
+    else:
+        notifier = None
+
     try:
         ChatUI(
             args.token_path, {
@@ -318,8 +340,7 @@ def main():
                 'menu': args.key_menu,
                 'up': args.key_up,
                 'down': args.key_down
-            }, col_scheme, palette_colors,
-            datetimefmt, args.disable_notifications
+            }, col_scheme, palette_colors, datetimefmt, notifier
         )
     except KeyboardInterrupt:
         sys.exit('Caught KeyboardInterrupt, exiting abnormally')
