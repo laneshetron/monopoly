d30becc95ddf78d388da4713314c94afad76e80d
laneshetron <laneshetron@gmail.com>
2016-10-11 23:55:45 -0400

    More Slack client setup

--- monopoly/Slack.py
+++ monopoly/Slack.py
@@ -1,6 +1,7 @@
 from Bank import g
 import websocket
 import requests
+import time
 import json
 
 domain = 'https://slack.com'
@@ -13,16 +14,30 @@ url = '{0}/{1}?token={2}&simple_latest=true&no_unreads=true'.format(domain, endp
 class SlackClient:
     def __init__(self):
         self.up = False
+        self.ws = None
+
+    def send(self, text, channel):
+        if self.ws:
+            message = json.dumps({'id': 1, 'type': 'message', 'channel': channel, 'text': text})
+            self.ws.send(message)
+        else:
+            raise Exception('No websocket available.')
 
     def on_message(self, ws, message):
         message = json.loads(message)
-        if message['type'] == 'hello':
-            self.up = True
-        elif message['type'] == 'error':
-            print('Error encountered: {0}'.format(message['error']))
+        print(message)
+        if 'type' in message:
+            if message['type'] == 'hello':
+                self.up = True
+            elif message['type'] == 'error':
+                print('Error encountered: {1}'.format(message['error']))
+            elif message['type'] == 'message' and 'reply_to' not in message:
+                # Consume message
+                print(message['text'])
 
     def on_error(self, ws, error):
         print(error)
+        self.connect()
 
     def on_close(self, ws):
         print('Connection to Slack closed. Reopening...')
@@ -33,16 +48,22 @@ class SlackClient:
         print('Connection to Slack established.')
 
     def connect(self):
-        res = requests.get(url).json()
-        if 'url' in res:
-            ws = websocket.WebSocketApp(res['url'], on_message=self.on_message,
-                                                    on_error=self.on_error,
-                                                    on_open=self.on_open,
-                                                    on_close=self.on_close)
-            ws.run_forever()
-        else:
-            # connection error
-            print('connection error')
+        while not self.up:
+            try:
+                res = requests.get(url).json()
+                if 'url' in res:
+                    self.ws = websocket.WebSocketApp(res['url'], on_message=self.on_message,
+                                                            on_error=self.on_error,
+                                                            on_open=self.on_open,
+                                                            on_close=self.on_close)
+                    self.ws.run_forever()
+                else:
+                    # connection error
+                    print('Connection error.', res)
+            except Exception as e:
+                print('Exception caught:', e)
+                print('Retrying in 1 second...')
+                time.sleep(1)
 
 def main(uptime, queues, **kwargs):
     g.uptime = uptime
