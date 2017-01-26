fe521401572f45a2fdd07597175cb6fd31ef7ede
Lane Shetron <laneshetron@gmail.com>
2017-01-26 12:52:49 -0500

    Final setup cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(name='monopoly',
       version='0.1.0',
-      description='A karma bot for Hangouts and IRC.',
+      description='A karma bot for Hangouts, Slack, and IRC.',
       long_description=readme(),
       url='https://github.com/laneshetron/monopoly.git',
       author='Lane Shetron',
@@ -15,7 +15,4 @@ setup(name='monopoly',
       packages=['monopoly'],
       install_requires=['hangups==0.1.0-monopoly', 'websocket-client', 'fuzzywuzzy', 'python-Levenshtein'],
       dependency_links=['https://github.com/laneshetron/hangups/archive/v0.1.0-monopoly.zip#egg=hangups-0.1.0-monopoly'],
-      include_package_data=True,
-      zip_safe=False)
-
-# Include setup of custom hangups branch
+      include_package_data=True)
